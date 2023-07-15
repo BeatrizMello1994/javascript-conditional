f4efcd25ee8b9aa48bb7bf08bf42900757c9921a
beatrizcandidodemello@gmail.com <beatrizcandidodemello@gmail.com>
2023-07-14 21:55:37 -0300

    aula 2 laços de repetiçao

---
+++ aula2/exe1.js
@@ -0,0 +1,62 @@
+let prompt = require('prompt-sync')();
+
+// let nomes = ["alana", "alexia", "samanta", "odirlei"]
+// // 0           1       2           3
+
+// for (let contador = 0; contador < nomes.length; contador++) {
+//     const cadaElemento = nomes[contador];
+//     console.log(cadaElemento);
+// }
+
+// nomes.forEach ((cadaElemento,posicao) => {
+//     console.log(posicao, cadaElemento);
+// });
+
+
+
+
+// const frutas = ["Pera","Uva","Maça","Salada Mista"]
+
+// for (let contador = 0; contador < frutas.length; contador++) {
+//     const cadaElemento = frutas[contador];
+//     console.log(contador,cadaElemento);
+// }
+
+
+
+// condiçao = salario = salario + 100; 
+// let salario = 1000;
+// let contador = 0;
+
+
+// while (salario < 5000) {
+//     salario =+ 100
+//     contador++;    
+
+//     console.log("o seu salario ainda é R$:" + salario);
+// }
+
+// console.log(contador);
+
+
+// let salario = 1000;
+
+
+
+
+
+let senha;
+
+do {
+    senha = prompt("digite a senha: " )
+
+    if ( senha != "123") {
+
+        console.log("senha incorreta");
+    }
+
+
+
+
+} while (senha != "123");
+console.log("senha correta");
